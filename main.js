7b58d81e4b5de26feac21a064cd084de757d0f0d
Rashi Ratan <rashiiratan@gmail.com>
2024-11-03 18:10:49 -0500

    Fetch product data and display basic information

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ fetch('https://www.course-api.com/javascript-store-products')
         return response.json(); // Parse the response as JSON
         
     })
+    .then(products => {
+        products.forEach(product => {
+            const fields = product.fields; // Access the fields object
+            const imageUrl = fields.image[0].url; //access images url in array
+
+            const listItem = document.createElement('li'); //create list item
+            //Displaying product info
+            const productInfo = document.createTextNode(`Product: ${fields.name}, Price: $${fields.price}, Company: ${fields.company}`);
+            listItem.appendChild(productInfo); //adding to list item
+
+            const img = document.createElement('img'); //creating image element
+            img.src = imageUrl;
+            img.alt = fields.name;
+            listItem.appendChild(img); //adding image to list item   
+           
+            productList.appendChild(listItem); //adding list item to list
+            });
+        })
